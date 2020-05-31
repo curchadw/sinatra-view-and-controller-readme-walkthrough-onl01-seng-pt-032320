ec28ce58194110580827dc441d6be3da67dde937
Curtis Chadwell <laurencechadwell@gmail.com>
2020-05-31 19:36:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
 
   get '/friends' do
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-    
+
     erb :friends
-    
+
   end
 end
